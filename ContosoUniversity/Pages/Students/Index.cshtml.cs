b18d1e92aa189733decef037d580eac5db8cc868
Kevin Gates <6576422+kevingates@users.noreply.github.com>
2022-09-23 16:48:57 -0400

    Part 3. Added Sorting and Searching features.

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -1,32 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Data;
-using ContosoUniversity.Models;
 
-namespace ContosoUniversity.Pages.Students
+namespace ContosoUniversity.Pages.Students;
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly SchoolContext _context;
+
+    public IndexModel(SchoolContext context)
     {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
+        _context = context;
+    }
 
-        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+    public string NameSort { get; set; }
+    public string DateSort { get; set; }
+    public string CurrentFilter { get; set; }
+    public string CurrentSort { get; set; }
+
+    public IList<Student> Students { get; set; }
+
+    public async Task OnGetAsync(string sortOrder, string searchString)
+    {
+        NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+        DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+        CurrentFilter = searchString;
+
+        IQueryable<Student> studentsIQ = from s in _context.Students
+                                         select s;
+        if (!String.IsNullOrEmpty(searchString))
         {
-            _context = context;
+            studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                                   || s.FirstMidName.Contains(searchString));
         }
 
-        public IList<Student> Student { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        switch (sortOrder)
         {
-            if (_context.Students != null)
-            {
-                Student = await _context.Students.ToListAsync();
-            }
+            case "name_desc":
+                studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                break;
+            case "Date":
+                studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                break;
+            case "date_desc":
+                studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                break;
+            default:
+                studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                break;
         }
+
+        Students = await studentsIQ.AsNoTracking().ToListAsync();
     }
 }
