4ca50c50230b938a362b2b58e1d162c8da3a4c2a
KevinGates <KevinGates@DESKTOP-CDOS7QB>
2022-09-23 12:09:48 -0400

    Add project files.

---
+++ ContosoUniversity/Models/SutdentVM.cs
@@ -0,0 +1,7 @@
+﻿public class StudentVM
+{
+    public int ID { get; set; }
+    public string LastName { get; set; }
+    public string FirstMidName { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+}
---
+++ ContosoUniversity/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Students
+{
+    public class CreateVMModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public CreateVMModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public StudentVM StudentVM { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var entry = _context.Add(new Student());
+            entry.CurrentValues.SetValues(StudentVM);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+    }
+}
