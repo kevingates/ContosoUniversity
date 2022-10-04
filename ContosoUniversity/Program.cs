f4af6e7662b65e45bc59202e7c9baaf1f2a75a13
Kevin Gates <6576422+kevingates@users.noreply.github.com>
2022-10-04 14:29:22 -0400

    Part 5 - EF Migrations

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -30,7 +30,6 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<SchoolContext>();
-    context.Database.EnsureCreated();
     DbInitializer.Initialize(context);
 }
 
